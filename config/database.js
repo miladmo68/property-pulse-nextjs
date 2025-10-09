b1c922de553ca3dfd0b1651b16d1414e3e5893f4
miladmo68 <miladmo68@gmail.com>
2025-10-09 13:22:16 -0400

    Updated

---
+++ config/database.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+let connected = false;
+
+const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  // If databse is already connected, dont connect again
+  if (connected) {
+    console.log("MongoDB is  connected");
+    return;
+  }
+
+  // Connect to MongoDB
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default connectDB;
